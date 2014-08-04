a6be6e08a564dc52154cd548b6c6a9096b1ea430
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2014-08-04 14:04:43 +0000

    added UTF-8 encoding and strict XHTML for index.html page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctv@1898 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ctv-server.R
+++ pkg/R/ctv-server.R
@@ -301,12 +301,13 @@ repos_update_views <- function(repos = ".",
 
   ## generate index HTML file
   if(is.character(index)) {
-    idx <- c("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">",
+    idx <- c("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">",
              "<html xmlns=\"http://www.w3.org/1999/xhtml\">",
              "",
 	     "<head>",
              paste0("  <title>", reposname, " Task Views</title>"),
              paste0("  <link rel=\"stylesheet\" type=\"text/css\" href=\"", css, "\" />"),
+            "  <meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\" />",
              "</head>",
 	     "",
 	     "<body>",
